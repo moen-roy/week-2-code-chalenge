91183be990adaabe6b6b8b10098dcc91e645e10d
moen <moenr49@gmail.com>
2025-06-17 23:53:11 +0300

    setting the querry selector have access to the 'guest-lit  form'

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".guest-details");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const input = document.getElementById("guestName");
+
+        const guestList = document.getElementById("guestDisplay");
+
+        const name = input.value.trim();
+
+         
+    });
     
 });
            
